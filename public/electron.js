18e4988b7697cc25a651e97019b6945c7b788270
yogathanh99 <vtthanh@apcs.vn>
2021-10-14 12:23:24 +0700

    [ADD] Integrate ReactJS to ElectronJS

---
+++ public/electron.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
+
+  // if (isDev) {
+  //   window.webContents.openDevTools({ mode: 'detach' });
+  // }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
